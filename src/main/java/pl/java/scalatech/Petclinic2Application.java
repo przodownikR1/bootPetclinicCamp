e07ea530ac66b9d5560d04dde5522366c28cd6fb
Slawomir Borowiec <przodownik@tlen.pl>
2016-09-04 13:09:15 +0200

    first migration step

---
+++ src/main/java/pl/java/scalatech/Petclinic2Application.java
@@ -0,0 +1,12 @@
+package pl.java.scalatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Petclinic2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Petclinic2Application.class, args);
+	}
+}
